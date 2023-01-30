c8f8349471997ccbfba454d1fbddf2a34516612c
samanemkul <91246654+samanemkul@users.noreply.github.com>
2023-01-30 14:07:48 +0545

    Updated sql file

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,17 +1,6 @@
-create database Petrolbikes
-Use petrolbikes
-go
- create table Pbike(
- id int not null primary key identity(1,1),
- name nvarchar(255) not null,
- bikeno int not null,
- year int not null
- );
- create table Ebike(
- id int not null primary key identity(1,1),
- name nvarchar(255) not null,
- bikeno int not null,
- year int not null
- );
- insert into Pbike(name,bikeno,year)
- VALUES ('TVS','7998','2021');
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[name]
+      ,[address]
+      ,[salary]
+  FROM [emp_details].[dbo].[emp]
